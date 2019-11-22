fe08eeded75e57f5e807f3c65394f956beaf0a74
Yaroslav Minakov <minakov.yaroslav92@gmail.com>
2019-11-22 19:34:10 +0300

    Changed class name; Changed enum value; Added new methods;

--- src/main/java/com/minakov/railwayticketbookingjdbc/config/DatabaseConfiguration.java
+++ src/main/java/com/minakov/railwayticketbookingjdbc/config/DatabaseConfiguration.java
@@ -5,13 +5,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-public enum DBProperties {
+public enum DatabaseConfiguration {
 
-    CONNECTION;
+    PROPERTY;
 
     private final Properties properties;
 
-    DBProperties() {
+    DatabaseConfiguration() {
         properties = new Properties();
         try (InputStream is = new FileInputStream("src/main/resources/database.properties")) {
             properties.load(is);
@@ -34,5 +34,17 @@ public enum DBProperties {
 
     public String getDriver() {
         return properties.getProperty("db.driver");
+    }
+
+    public String getType() {
+        return properties.getProperty("db.connection");
+    }
+
+    public String getDialect() {
+        return properties.getProperty("db.dialect");
+    }
+
+    public String getHbm2ddlAuto() {
+        return properties.getProperty("db.hbm2ddl_auto");
     }
 }
